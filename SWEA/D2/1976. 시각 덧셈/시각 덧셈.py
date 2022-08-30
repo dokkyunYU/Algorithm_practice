d92fb255d2c6e628ec776df5a687473f2f73088e
dokkyunYU <109258608+dokkyunYU@users.noreply.github.com>
2022-08-30 16:58:48 +0900

    [D2] Title: 시각 덧셈, Time: 159 ms, Memory: 58,260 KB -BaekjoonHub

---
+++ SWEA/D2/1976. 시각 덧셈/시각 덧셈.py
@@ -0,0 +1,3 @@
+for test_case in range(1, int(input()) + 1):
+    clock_list = list(map(int, input().split()))
+    print(f"#{test_case}", sum(clock_list[::2]) % 12 + sum(clock_list[1::2]) // 60, sum(clock_list[1::2]) % 60)
