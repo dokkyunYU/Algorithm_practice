e8744665ef8193e546e33be9f813699881de206c
dokkyunYU <109258608+dokkyunYU@users.noreply.github.com>
2022-09-06 18:57:25 +0900

    [Silver V] Title: 수 이어가기, Time: 96 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/2635. 수 이어가기/수 이어가기.py
@@ -0,0 +1,15 @@
+the_number = int(input())
+
+max_length = 0
+max_list = []
+for n in range(the_number+1):
+    numbers_list = [the_number]
+    numbers_list.append(n)
+    while numbers_list[-1] >= 0:
+        numbers_list.append(numbers_list[-2] - numbers_list[-1])
+    numbers_list.pop()
+    if len(numbers_list) >= max_length:
+        max_length = len(numbers_list)
+        max_list = numbers_list
+print(max_length)
+print(*max_list)
