ff6665b4667a14fe325872df1b904c4281337a31
dokkyunYU <109258608+dokkyunYU@users.noreply.github.com>
2022-09-07 20:43:58 +0900

    [Silver IV] Title: 개미, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/10158. 개미/개미.py
@@ -0,0 +1,19 @@
+width, height = map(int, input().split())
+p, q = map(int, input().split())
+walking_time = int(input())
+np = (
+    abs(width - p - (walking_time % width))
+    if (walking_time // width) & 1
+    else p + (walking_time % width)
+)
+nq = (
+    abs(height - q - (walking_time % height))
+    if (walking_time // height) & 1
+    else q + (walking_time % height)
+)
+if np > width:
+    np = width - (np % width)
+if nq > height:
+    nq = height - (nq % height)
+
+print(np, nq)
