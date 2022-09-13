6d7dbbfb5455a5a2e8e682a4e714a1a17527a6ca
dokkyunYU <109258608+dokkyunYU@users.noreply.github.com>
2022-09-14 00:06:41 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 중위순회, Time: 153 ms, Memory: 58,744 KB -BaekjoonHub

---
+++ SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.py
@@ -0,0 +1,27 @@
+def inorder_search(node_num):
+    global result_string
+    # 왼쪽 탐색 시도
+    try:
+        inorder_search(int(the_tree[node_num][2]))
+    except IndexError:
+        # 없을 경우 그냥 현재 값 입력 후 종료
+        result_string += the_tree[node_num][1]
+        return
+    # 위에서 입력 및 종료가 일어나지 않은 경우, 즉 왼쪽 자식 노드를 탐색하고 돌아온 경우
+    result_string += the_tree[node_num][1]
+    # 오른쪽 탐색 시도
+    try:
+        inorder_search(int(the_tree[node_num][3]))
+    except IndexError:
+        # 없다면 그냥 종료
+        return
+
+
+for test_count in range(1, 11):
+    node_count = int(input())
+    the_tree = [0]
+    result_string = ''
+    for _ in range(node_count):
+        the_tree.append(input().split())
+    inorder_search(1)
+    print(f"#{test_count}", result_string)
