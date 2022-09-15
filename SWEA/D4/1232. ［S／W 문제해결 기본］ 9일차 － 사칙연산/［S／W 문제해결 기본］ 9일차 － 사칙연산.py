44b9b5dfb4c4155b2aaaf7caf55c755499797cbc
dokkyunYU <109258608+dokkyunYU@users.noreply.github.com>
2022-09-15 16:14:41 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산, Time: 233 ms, Memory: 64,392 KB -BaekjoonHub

--- SWEA/D4/1232. ［S／W 문제해결 기본］ 9일차 － 사칙연산/［S／W 문제해결 기본］ 9일차 － 사칙연산.py
+++ SWEA/D4/1232. ［S／W 문제해결 기본］ 9일차 － 사칙연산/［S／W 문제해결 기본］ 9일차 － 사칙연산.py
@@ -2,18 +2,18 @@ def postorder(node_num):
     if len(calculate_tree[node_num]) > 1:
         postorder(int(calculate_tree[node_num][1]))
         postorder(int(calculate_tree[node_num][2]))
-        if calculate_tree[node_num][0] == "+":
-            calculate_stack.append(calculate_stack.pop() + calculate_stack.pop())
-        elif calculate_tree[node_num][0] == "-":
-            calculate_stack.append(-calculate_stack.pop() + calculate_stack.pop())
-        elif calculate_tree[node_num][0] == "*":
-            calculate_stack.append(calculate_stack.pop() * calculate_stack.pop())
-        else:
-            calculate_stack.append(1 / calculate_stack.pop() * calculate_stack.pop())
+        calculate_stack.append(
+            operator_dict[calculate_tree[node_num][0]](
+                calculate_stack.pop(), calculate_stack.pop())
+        )
     else:
         calculate_stack.append(int(calculate_tree[node_num][0]))
 
 
+operator_dict = {"+": lambda x, y: y + x,
+                 "-": lambda x, y: y - x,
+                 "*": lambda x, y: y * x,
+                 "/": lambda x, y: y / x}
 for test_count in range(1, 11):
     node_count = int(input())
     calculate_tree = [[] for _ in range(node_count + 1)]
