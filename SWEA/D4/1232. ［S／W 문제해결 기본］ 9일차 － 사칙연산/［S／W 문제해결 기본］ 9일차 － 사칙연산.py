4b62446ee38b32815110e921d719ad327b0c134a
dokkyunYU <109258608+dokkyunYU@users.noreply.github.com>
2022-09-15 15:54:07 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산, Time: 212 ms, Memory: 64,376 KB -BaekjoonHub

---
+++ SWEA/D4/1232. ［S／W 문제해결 기본］ 9일차 － 사칙연산/［S／W 문제해결 기본］ 9일차 － 사칙연산.py
@@ -0,0 +1,25 @@
+def postorder(node_num):
+    if len(calculate_tree[node_num]) > 1:
+        postorder(int(calculate_tree[node_num][1]))
+        postorder(int(calculate_tree[node_num][2]))
+        if calculate_tree[node_num][0] == "+":
+            calculate_stack.append(calculate_stack.pop() + calculate_stack.pop())
+        elif calculate_tree[node_num][0] == "-":
+            calculate_stack.append(-calculate_stack.pop() + calculate_stack.pop())
+        elif calculate_tree[node_num][0] == "*":
+            calculate_stack.append(calculate_stack.pop() * calculate_stack.pop())
+        else:
+            calculate_stack.append(1 / calculate_stack.pop() * calculate_stack.pop())
+    else:
+        calculate_stack.append(int(calculate_tree[node_num][0]))
+
+
+for test_count in range(1, 11):
+    node_count = int(input())
+    calculate_tree = [[] for _ in range(node_count + 1)]
+    for _ in range(node_count):
+        idx = input().split()
+        calculate_tree[int(idx[0])] += idx[1:]
+    calculate_stack = []
+    postorder(1)
+    print(f"#{test_count}", int(calculate_stack[0]))
