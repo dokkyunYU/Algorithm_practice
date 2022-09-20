d3ae2a1fecf78deb43444dbfc010d7aae6bbc4c3
dokkyunYU <109258608+dokkyunYU@users.noreply.github.com>
2022-09-20 17:39:56 +0900

    [D3] Title: 이진수 표현, Time: 1,264 ms, Memory: 63,000 KB -BaekjoonHub

---
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.py
@@ -0,0 +1,7 @@
+for test_count in range(1, int(input()) + 1):
+    n, m = map(int, input().split())
+    bit_counter = 0
+    for i in range(n):
+        if m & 1 << i:
+            bit_counter += 1
+    print(f"#{test_count}", "ON" if bit_counter == n else "OFF")
