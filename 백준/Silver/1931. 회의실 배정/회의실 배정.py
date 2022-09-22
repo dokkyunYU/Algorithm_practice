9bbbf41cbf910d944a5ec486510a0ab3d8e8973c
dokkyunYU <109258608+dokkyunYU@users.noreply.github.com>
2022-09-22 15:15:40 +0900

    [Silver I] Title: 회의실 배정, Time: 4212 ms, Memory: 51616 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,15 +1,10 @@
-n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
-
-arr.sort(key=lambda x: x[0])
-arr.sort(key=lambda x: x[1])
-
+meeting_count = int(input())
+meeting_info = [tuple(map(int, input().split())) for _ in range(meeting_count)]
+meeting_info.sort(key=lambda x: (x[1], x[0]))
 end = 0
-count = 0
-
-for i in arr:
+schedule_count = 0
+for i in meeting_info:
     if i[0] >= end:
         end = i[1]
-        count += 1
-
-print(count)
+        schedule_count += 1
+print(schedule_count)
