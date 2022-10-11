a8f4d413e3d2a77ff69c6dd03c3f6c4cb199a7e0
dokkyunYU <109258608+dokkyunYU@users.noreply.github.com>
2022-10-11 20:57:49 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.63 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -0,0 +1,15 @@
+def solution(board, moves):
+    answer = []
+    answer_cnt = 0
+    b_size = len(board)
+    for k in moves:
+        for i in range(b_size):
+            if board[i][k-1]:
+                if answer == [] or answer[-1] != board[i][k-1]:
+                    answer.append(board[i][k-1])
+                elif answer[-1] == board[i][k-1]:
+                    answer.pop()
+                    answer_cnt += 2
+                board[i][k-1] = 0
+                break
+    return answer_cnt
