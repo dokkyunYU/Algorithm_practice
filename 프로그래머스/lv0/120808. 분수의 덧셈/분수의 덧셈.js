dad7bd80d63baf3271385632a5da4a63b156415a
dokkyunYU <109258608+dokkyunYU@users.noreply.github.com>
2022-10-21 15:06:05 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.20 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,6 +1,6 @@
 function dividemax(num1, num2) {
-    let number = (num1 > num2) ? parseInt(Math.sqrt(num2)) : parseInt(Math.sqrt(num1))
-    for (let i = num1; i > 1; i--) {
+    let number = (num1 > num2) ? num2 : num1
+    for (let i = number; i > 1; i--) {
         if (!(num1 % i) && !(num2 % i))
             return [num1 / i, num2 / i]
     }
