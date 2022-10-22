3ca31a25583602e58eb86477c59d4d20d648c5da
dokkyunYU <109258608+dokkyunYU@users.noreply.github.com>
2022-10-22 23:44:00 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(void){
+    int n = 0;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++)
+            cout << "*";
+        cout << endl;
+    }
+    return 0;
+}
