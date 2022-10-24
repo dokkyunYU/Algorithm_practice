5be797272d23d4dd8fa1a62a74dcf3f506b04c02
dokkyunYU <109258608+dokkyunYU@users.noreply.github.com>
2022-10-24 17:48:57 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+    printf("\\    /\\\n");
+    printf(" )  ( ')\n");
+    printf("(  /  )\n");
+    printf(" \\(__)|\n");
+    return 0;
+}
