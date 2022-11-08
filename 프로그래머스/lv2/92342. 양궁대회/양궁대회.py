de2e30dab5aef931f1f1041517fc524356692fe7
dokkyunYU <109258608+dokkyunYU@users.noreply.github.com>
2022-11-08 22:08:32 +0900

    [level 2] Title: 양궁대회, Time: 0.85 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/92342. 양궁대회/양궁대회.py
@@ -0,0 +1,54 @@
+def shooting(bullseye, arrows, rivalscore, nowlocation=0, totalsum=0, resultlist=[]):
+    global maxsum, maxlist
+    if arrows == 0 or nowlocation == 10:
+        if (totalsum - rivalscore) > maxsum:
+            maxlist = []
+            maxsum = totalsum - rivalscore
+            maxlist.append(resultlist + [arrows])
+        elif (totalsum - rivalscore) == maxsum:
+            maxlist.append(resultlist + [arrows])
+        return
+    if arrows > bullseye[nowlocation]:
+        if bullseye[nowlocation]:
+            shooting(
+                bullseye,
+                arrows - bullseye[nowlocation] - 1,
+                rivalscore - (10 - nowlocation),
+                nowlocation + 1,
+                totalsum + 10 - nowlocation,
+                resultlist + [bullseye[nowlocation] + 1],
+            )
+        else:
+            shooting(
+                bullseye,
+                arrows - bullseye[nowlocation] - 1,
+                rivalscore,
+                nowlocation + 1,
+                totalsum + 10 - nowlocation,
+                resultlist + [bullseye[nowlocation] + 1],
+            )
+    shooting(bullseye, arrows, rivalscore, nowlocation + 1, totalsum, resultlist + [0])
+
+
+def solution(n, info):
+    global maxsum, maxlist
+    maxsum = 0
+    maxlist = []
+    infoscore = 0
+    for i in range(11):
+        if info[i]:
+            infoscore += 10 - i
+    shooting(info, n, infoscore)
+    answer=[]
+    max_length=0
+    for i in maxlist:
+        length = len(i)
+        if max_length != max(max_length, length):
+            max_length = length
+            answer = i
+    if len(answer) < 11:
+        answer += [0] * (11 - len(answer))
+    if maxsum == 0:
+        return [-1]
+    print(maxlist)
+    return answer
