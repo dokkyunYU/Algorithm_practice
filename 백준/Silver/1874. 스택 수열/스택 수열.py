9cb53150fa73cac095031f8f912be7ef160697e6
dokkyunYU <109258608+dokkyunYU@users.noreply.github.com>
2023-06-13 15:32:09 +0900

    [Silver II] Title: 스택 수열, Time: 260 ms, Memory: 134244 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,25 @@
+num_list = []
+n = int(input())
+stack = [0]
+last = 1
+result = []
+for _ in range(n):
+    num_list.append(int(input()))
+
+for i in num_list:
+    if i == stack[-1]:
+        stack.pop()
+        result.append("-")
+    elif i < stack[-1]:
+        print("NO")
+        break
+    else:
+        while last <= i:
+            stack.append(last)
+            result.append("+")
+            last += 1
+        stack.pop()
+        result.append("-")
+else:
+    for i in result:
+        print(i)
