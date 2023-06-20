51d634fe110d71c0f34ce0c260d8fa827788b9a3
dokkyunYU <109258608+dokkyunYU@users.noreply.github.com>
2023-06-20 18:59:48 +0900

    [Silver I] Title: 점프, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1890. 점프/점프.py
@@ -0,0 +1,20 @@
+n = int(input())
+board = []
+for _ in range(n):
+    board.append(list(map(int, input().split())))
+board2 = [[0 for _ in range(n)] for _ in range(n)]
+board2[0][0] = 1
+for i in range(n):
+    for j in range(n):
+        num = board[i][j]
+        if i == n-1 and j == n-1:
+            break
+        try:
+            board2[i + num][j] += board2[i][j]
+        except IndexError:
+            pass
+        try:
+            board2[i][j + num] += board2[i][j]
+        except IndexError:
+            pass
+print(board2[n-1][n-1])
