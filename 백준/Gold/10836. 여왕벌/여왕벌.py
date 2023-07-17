84d60c78ba21f3115dd3d4b4f9abd3f22e8a1706
dokkyunYU <109258608+dokkyunYU@users.noreply.github.com>
2023-07-17 16:59:18 +0900

    [Gold IV] Title: 여왕벌, Time: 264 ms, Memory: 34724 KB -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.py
+++ 백준/Gold/10836. 여왕벌/여왕벌.py
@@ -6,25 +6,23 @@ board = [[1 for _ in range(length)] for _ in range(length)]
 grow_board_dict = {}
 for day in range(days):
     idx = readline()
-    grow_board = grow_board_dict.get(idx)
-    if not grow_board:
-        grow_board = [[0 for _ in range(length)] for _ in range(length)]
+    grow_lists = grow_board_dict.get(idx)
+    if not grow_lists:
         zero, one, two = map(int, idx.split())
         grow_list = [0 for _ in range(zero)] + [1 for _ in range(one)] + [2 for _ in range(two)]
         grow_len = 2 * length - 1
-        for i in range(length):
-            grow_board[0][length - 1 - i] = grow_list[grow_len - 1 - i]
-            grow_board[length - 1 - i][0] = grow_list[i]
-            if i == grow_len - 1 - i:
-                break
-        for i in range(1, length):
-            for j in range(1, length):
-                grow_board[i][j] = grow_board[0][j]
-        grow_board_dict.update({idx: grow_board})
-    
+        grow_list2 = grow_list[grow_len // 2:]
+        grow_board_dict.update({idx: [grow_list, grow_list2]})
+    else:
+        grow_list, grow_list2 = grow_lists
     for i in range(length):
-        for j in range(length):
-            board[i][j] += grow_board[i][j]
-
+        board[0][length - 1 - i] += grow_list[grow_len - 1 - i]
+        board[length - 1 - i][0] += grow_list[i]
+        if i == grow_len - 1 - i:
+            board[0][0] -= grow_list[i]
+    for i in range(1, length):
+        for j in range(1, length):
+            board[i][j] += grow_list2[j]
+    
 for line in board:
     print(*line)
