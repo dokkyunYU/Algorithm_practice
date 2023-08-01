e9217fd2371e6e17e45f624cf2cef3919bb015b0
dokkyunYU <109258608+dokkyunYU@users.noreply.github.com>
2023-08-01 14:31:10 +0900

    [Silver I] Title: Z, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1074. Z/Z.py
@@ -0,0 +1,12 @@
+N, r, c = map(int, input().split())
+
+start_sum = 0
+
+for i in range(N - 1, 0, -1):
+    num = 2**i
+    num2 = num*num
+    start_sum += ((r // num) * 2 + c // num) * num2
+    r = r % num
+    c = c % num
+
+print(start_sum + 2 * r + c)
