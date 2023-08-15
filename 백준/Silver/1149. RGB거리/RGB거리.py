af0114acab3d8ca29ec7b582960b2b848d061384
dokkyunYU <109258608+dokkyunYU@users.noreply.github.com>
2023-08-15 17:18:49 +0900

    [Silver I] Title: RGB거리, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -6,12 +6,9 @@ cost = []
 for _ in range(num):
     cost.append(list(map(int, readline().split())))
 
-dp = [[0, 0, 0] for _ in range(num)]
-dp[0] = cost[0]
-
 for i in range(1, num):
-    dp[i][0] = cost[i][0] + min(dp[i-1][1], dp[i-1][2])
-    dp[i][1] = cost[i][1] + min(dp[i-1][0], dp[i-1][2])
-    dp[i][2] = cost[i][2] + min(dp[i-1][0], dp[i-1][1])
+    cost[i][0] += min(cost[i-1][1], cost[i-1][2])
+    cost[i][1] += min(cost[i-1][0], cost[i-1][2])
+    cost[i][2] += min(cost[i-1][0], cost[i-1][1])
 
-print(min(dp[num - 1]))
+print(min(cost[num - 1]))
