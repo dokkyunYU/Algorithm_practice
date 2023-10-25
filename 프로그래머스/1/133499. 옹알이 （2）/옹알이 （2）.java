b74f44244b49ff57b077f11db2b358ac894b2ce2
dokkyunYU <109258608+dokkyunYU@users.noreply.github.com>
2023-10-25 15:28:10 +0900

    [level 1] Title: 옹알이 (2), Time: 3.35 ms, Memory: 64.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        int size = babbling.length;
+        String[] canSpeak = {"aya", "ye", "woo", "ma"};
+        for (String i : babbling) {
+            for(String speaking : canSpeak) {
+                if (i.contains(speaking + speaking)) break;
+                i = i.replace(speaking, "-");
+            }
+            if (i.replace("-", "").equals("")) answer++;
+        }
+        return answer;
+    }
+}
