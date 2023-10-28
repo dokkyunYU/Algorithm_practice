af3dfbfe75d542c3c6277683a0ec76685c69687d
dokkyunYU <109258608+dokkyunYU@users.noreply.github.com>
2023-10-28 18:30:30 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.19 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,7 +1,6 @@
 class Solution {
     public long solution(long n) {
         double x = Math.sqrt(n);
-        if (Math.round(x) == x) return (long) ((x+1) * (x+1));
-        else return -1;
+        return (Math.floor(x) == x) ? (long) ((x+1) * (x+1)) : -1;
     }
 }
