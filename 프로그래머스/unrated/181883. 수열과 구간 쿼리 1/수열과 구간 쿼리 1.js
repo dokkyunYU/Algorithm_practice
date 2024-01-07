8cc4d5904439b955fc519bfd028ada6c36a41fa4
dokkyunYU <109258608+dokkyunYU@users.noreply.github.com>
2024-01-07 17:44:17 +0900

    [level unrated] Title: 수열과 구간 쿼리 1, Time: 2.48 ms, Memory: 35.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
@@ -0,0 +1,8 @@
+function solution(arr, queries) {
+    const answer = [...arr];
+    queries.forEach(([s,e]) => {
+        for (let i = s; i <= e; ++i)
+            ++answer[i];
+    })
+    return answer;
+}
