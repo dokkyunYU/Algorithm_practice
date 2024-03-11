802194b3690bb920f2b0fdf4612e2271dd34465c
dokkyunYU <109258608+dokkyunYU@users.noreply.github.com>
2024-03-11 10:38:52 +0900

    [level 1] Title: 모의고사, Time: 8.38 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -4,11 +4,11 @@ function solution(answers) {
         [2,1,2,3,2,4,2,5],
         [3,3,1,1,2,2,4,4,5,5]
     ]
-    const answer = [0,0,0]
+    const answer = Array(supoArr.length).fill(0)
     for (const idx in answers) {
-        answer[0] += supoArr[0][idx % supoArr[0].length] === answers[idx]
-        answer[1] += supoArr[1][idx % supoArr[1].length] === answers[idx]
-        answer[2] += supoArr[2][idx % supoArr[2].length] === answers[idx]
+        for (const supoIdx in supoArr) {
+            answer[supoIdx] += supoArr[supoIdx][idx % supoArr[supoIdx].length] === answers[idx]    
+        }
     }
     const maxValue = Math.max(...answer)
     return answer.reduce((acc,cur,idx) => cur === maxValue ? [...acc, idx + 1] : acc, []);
