20f56d1ceac96df0bc55c0ef086a5a4b6110e3e0
dokkyunYU <109258608+dokkyunYU@users.noreply.github.com>
2024-05-14 11:32:39 +0900

    [level 2] Title: 주식가격, Time: 15.21 ms, Memory: 43.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584?language=javascript) 
 
 ### 성능 요약
 
-메모리: 44.5 MB, 시간: 17.09 ms
+메모리: 43.1 MB, 시간: 15.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 14일 11:28:43
+2024년 05월 14일 11:32:37
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,12 +1,11 @@
 function solution(prices) {
-    const answer = [], stack = [], end = prices.length - 1
+    const answer = Array(prices.length).fill(0), stack = [], end = prices.length - 1
     for (let idx in prices) {
         while (stack.length && prices[stack.at(-1)] > prices[idx]) {
             const i = stack.pop()
             answer[i] = idx - i
         }
         stack.push(idx)
-        answer.push(0)
     }
     for (let i of stack) {
         answer[i] = end - i
